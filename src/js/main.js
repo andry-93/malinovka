4b476d819127d8455f34ccd634503af14df1461f
Andrei <andry-93@bk.ru>
2019-04-06 12:36:17 +0300

    Template

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+  var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    effect: 'fade',
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  });
+});
